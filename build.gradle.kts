fda3706801bff7126e6a6efb3e991e7d70111489
Gilson Rodrigues <gilson_rodrigues@hotmail.com>
2026-09-21 17:23:52 -0300

    Criei o teste para incluir um pet

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.testng:testng:7.12.0")
+    testImplementation("io.rest-assured:rest-assured:6.0.1")
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
 }
